f8ac620f28f908a60e005f1ea55d0aacdd7a197d
KHAI ZHANG (ITA-ISDC-ISD-OOCLL/ZHA) <ZHANGKH@OOCL.COM>
2019-07-08 17:21:56 +0800

    second commit

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -4,9 +4,12 @@ function collectSameElements(collectionA, collectionB) {
   var result=[];
    for(let i=0;i<collectionA.length;i++){
       for(let j=0;j<collectionB.length;j++){
-        for(let k=0;k<collectionB[j].length;j++){
-          if(collectionA[I]==collectionB[j])
-        }
+          var arr=collectionB[j];
+          for(let k=0;k<arr.length;k++){
+            if(collectionA[i]==arr[k]){
+              result.push(collectionA[i]);
+            }
+          }
       }
 
     }
